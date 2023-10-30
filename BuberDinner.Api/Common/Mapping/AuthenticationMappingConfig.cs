d4c2ff3e936961b2294d093d6aaefba8dc96cea0
Wanderlei Santos <wanderleisantosdias@gmail.com>
2023-10-30 12:41:22 -0300

    feat: Created MenuResponse

--- BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
+++ BuberDinner.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -1,7 +1,6 @@
-using BuberDinner.Application.Authentication.Commands.Register;
 using BuberDinner.Application.Authentication.Common;
-using BuberDinner.Application.Authentication.Queries.Login;
 using BuberDinner.Contracts.Authentication;
+using BuberDinner.Domain;
 using Mapster;
 
 namespace BuberDinner.Api.Common.Mapping;
@@ -10,9 +9,9 @@ public class AuthenticationMappingConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config.NewConfig<RegisterRequest, RegisterCommand>();
-        config.NewConfig<LoginRequest, LoginQuery>();
         config.NewConfig<AuthenticationResult, AuthenticationResponse>()
-            .Map(dest => dest, src => src.User);
+            .Map(dest => dest.Token, src => src.Token)
+            .Map(dest => dest, src => src.User)
+            .Map(dest => dest.Id, src => UserId.Create(src.User.Id.Value));
     }
 }
---
+++ BuberDinner.Application/Menus/Commands/CreateMenu/CreateMenuCommandHandler.cs
@@ -0,0 +1,13 @@
+using BuberDinner.Domain.Menus;
+using ErrorOr;
+using MediatR;
+
+namespace BuberDinner.Application.Menus.Commands.CreateMenu;
+
+public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, ErrorOr<Menu>>
+{
+    public Task<ErrorOr<Menu>> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
